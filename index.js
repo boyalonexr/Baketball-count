e40782d59ecb32c52bf37f538d14f612cd4cead3
CHIDUBEM VICTOR UGWU <victorugwu9772@gmail.com>
2024-07-04 21:44:23 +0100

    project done

---
+++ index.js
@@ -0,0 +1,44 @@
+let homeScore = document.getElementById("home-score");
+let awayScore = document.getElementById("away-score")
+
+function homeAdd1() {
+    let text = 0 + 1;
+    homeScore.textContent = "0" + text;
+}
+
+function homeAdd2() {
+    let text = 0 + 2;
+    homeScore.textContent = "0" + text;
+}
+
+function homeAdd3() {
+    let text = 0 + 3;
+    homeScore.textContent = "0" + text;
+}
+
+function awayAdd1() {
+    let text = 0 + 1;
+    awayScore.textContent = "0" + text;
+}
+
+function awayAdd2() {
+    let text = 0 + 2;
+    awayScore.textContent = "0" + text;
+}
+
+function awayAdd3() {
+    let text = 0 + 3;
+    awayScore.textContent = "0" + text;
+}
+
+function reset() {
+    homeScore.textContent = 0;
+    awayScore.textContent = 0;
+    
+}
+
+
+
+
+
+// console.log(dome)
